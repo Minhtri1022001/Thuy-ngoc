09cc1acb499f998d925230b318a4d154658c8187
Minhtri1022001 <88764480+Minhtri1022001@users.noreply.github.com>
2021-08-11 14:53:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const CONFIG = {
     introTitle: 'Bae à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường(cứ dừng lại thoi-.-)
+    Mà sao em cứ đi nhầm đường
+    (cứ dừng lại thoi 😑)
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi 🤭',
@@ -9,11 +10,11 @@ const CONFIG = {
     desc: 'Về bên anh em nhé! ',
     btnYes: 'Tạm chấp nhận 🤭🤭 ',
     btnNo: 'Không, ghétttt, không thèm về',
-    question:'Tha thứ cho anh bae nhé <3 sao em lại yêu anh thế nhỉ 🤭🤭 ',
+    question:'Tha thứ cho anh bae nhé 🤭🤭 ',
     btnReply: 'Gửi cho anh <3',
-    reply: '',
-    mess: 'Anh biết mà 🥰. Yêu công chúa của anh nhiềuu 😘😘',
+    reply: 'Vâng ạ',
+    mess: 'Không muốn xa em đâuu🥰. Yêu công chúa của anh nhiềuu lắm 😘😘',
     messDesc: 'Yêu anh honggg.',
-    btnAccept: 'Okeeee <3',
+    btnAccept: 'Em yêu anhhh <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
